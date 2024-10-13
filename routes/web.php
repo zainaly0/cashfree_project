88facbf890547f2586c00f7cad01cd884ca47254
zainaly0 <zaidk4076@gmail.com>
2024-10-13 17:36:21 +0530

    this is complete but it si not working

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 
-Route::get('/', [PaymentController::class, 'index']);
+// Route::get('/', [PaymentController::class, 'index']);
 
-Route::post("/pay", [PaymentController::class, "initialPayment"])->name("payment.pay");
-Route::get('/success/{orderId}', [PaymentController::class, 'PaymentSuccess'])->name('payment.success');
+// Route::post("/pay", [PaymentController::class, "initialPayment"])->name("payment.pay");
+// Route::get('/success/{orderId}', [PaymentController::class, 'PaymentSuccess'])->name('payment.success');

1af053539dc1297df2368414b0f5cef31d3ce12c
zainaly0 <zaidk4076@gmail.com>
2024-10-13 19:01:12 +0530

    payment store function is complete

---
+++ app/Http/Controllers/CashfreePaymentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CashfreePaymentController extends Controller
+{
+    public function create(){
+        return view('Two.payment-create');
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'email' =>'required',
+            'mobile' => 'required',
+            'amount' => 'required'
+        ]);
+
+        $url = "https://sandbox.cashfree.com/pg/orders";
+
+        $headers = array(
+            'Content-Type: application/json',
+            'x-api-version: 2023-08-01',
+            'x-client-id:'.env('CASHFREE_API_ID'),
+            'x-client-secret: '.env('CASHFREE_API_SECRET')
+        );
+
+        $data = json_encode([
+            'order_id' => 'order_'.rand(11111111, 99999999),
+            'order_amount' => $validated['amount'],
+            'order_currency' => 'INR',
+            'customer_details' => [
+                "customer_id" => 'customer_'.rand(11111111, 99999999),
+                'customer' => $validated['name'],
+                'customer_email' => $validated['email'],
+                'customer_phone' => $validated['mobile']
+            ],
+            'order_meta' => [
+                'return_url' => 'http://127.0.0.1:8000/cashfree/payments/success/?order_id={order_id}&order_token={order_token}'
+            ]
+        ]);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        $resp = curl_exec($curl);
+
+        curl_close($curl);
+
+        return redirect()->to(json_decode($resp)->payment_link);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\CashfreePaymentController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PaymentController;
+// use App\Http\Controllers\PaymentController;
 
+
+// FIRST WAY- NOT WORKING
 // Route::get('/', [PaymentController::class, 'index']);
-
 // Route::post("/pay", [PaymentController::class, "initialPayment"])->name("payment.pay");
 // Route::get('/success/{orderId}', [PaymentController::class, 'PaymentSuccess'])->name('payment.success');
+
+
+Route::get('/cashfree/payments/create', [CashfreePaymentController::class,'create'])->name('callback');
+Route::post('/cashfree/payments/store', [CashfreePaymentController::class,'store'])->name('store');
+Route::get('/cashfree/payments/success', [CashfreePaymentController::class,'success'])->name('success');
